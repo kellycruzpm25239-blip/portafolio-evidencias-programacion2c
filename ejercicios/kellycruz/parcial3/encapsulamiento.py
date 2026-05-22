1f0da109a4c1d58c30b750f271f83f17ebcb2b1c
Kelly Cruz <kelly.cruz_pm225239@cecyte.edu.mx>
2026-05-22 09:35:01 -0600

    agregamdo herencia a parcial 3

---
+++ ejercicios/kellycruz/parcial3/encapsulamiento.py
@@ -0,0 +1,26 @@
+class CuentaBancaria:
+    def __init__(self, saldo):
+        self._saldo = saldo
+
+    def set_saldo(self, saldo):
+        self._saldo = saldo
+
+    def get_saldo(self):
+        return self._saldo
+        
+    def depositar(self, cantidad):
+        self._saldo += cantidad
+
+    def retirar(self, cantidad):
+        if cantidad <= self._saldo:
+            self._saldo -= cantidad
+        else:
+            print("fondos insuficientes")
+
+    def mostrar_saldo(self):
+        print(f"saldo: {self._saldo}")
+
+cuenta = CuentaBancaria(5000)
+cuenta.set_saldo(-5000)
+print(cuenta.get_saldo())
+cuenta.mostrar_saldo()
